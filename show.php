5cac39c42783648a394be1fe6696cfedfec807d5
swnx-3177 <73655917+swnx-3177@users.noreply.github.com>
2020-11-20 13:32:47 +0700

    Update show.php

--- show.php
+++ show.php
@@ -74,7 +74,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
           <input type="hidden" name="name" value="<?php echo $Result['Name']; ?>" />
           <input type="hidden" name="comment" value="<?php echo $Result['Comment']; ?>" />
           <input type="hidden" name="link" value="<?php echo $Result['Link']; ?>" />
-        <button type="submit" "btn btn-outline-warning" >Edit</button>
+        <button type="submit" class="btn btn-outline-warning" >Edit</button>
         </form>
       </td>
     </tr>
